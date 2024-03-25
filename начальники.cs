5bcc82cbd9e6fe2afc1c71f6a20ed40cf0a8780d
Kuzina-Varya <145117807+Kuzina-Varya@users.noreply.github.com>
2024-03-25 23:58:46 +0600

    Create начальники.cs

---
+++ начальники.cs
@@ -0,0 +1,24 @@
+﻿namespace началиники
+{
+    public class Base
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                string str = Console.ReadLine();
+                while (str != "END")
+                {
+
+
+                }
+
+            }
+           
+        }
+    }
+}
